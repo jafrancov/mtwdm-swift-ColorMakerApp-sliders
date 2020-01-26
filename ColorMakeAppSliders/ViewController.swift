5256af93b88ed8326acb10a9a80d23f98acfe133
Alejandro Franco <jafrancov@gmail.com>
2020-01-26 13:42:39 -0600

    Change view background color with sliders

--- ColorMakeAppSliders/ViewController.swift
+++ ColorMakeAppSliders/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sliderR: UISlider!
+    @IBOutlet weak var sliderG: UISlider!
+    @IBOutlet weak var sliderB: UISlider!
+    @IBOutlet weak var viewColor: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func changeViewBackground(_ sender: Any) {
+        let r:Float = self.sliderR!.value
+        let g:Float = self.sliderG!.value
+        let b:Float = self.sliderB!.value
+        self.viewColor.backgroundColor = UIColor(
+            displayP3Red: CGFloat(r),
+            green: CGFloat(g),
+            blue: CGFloat(b),
+            alpha: 1)
+    }
 }
 
